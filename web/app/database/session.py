5b6cabd6f46e22ecdb8be0f1155017c6e327f416
root <root@localhost.localdomain>
2026-08-15 07:07:57 +0500

    fix async postgres connection

--- web/app/database/session.py
+++ web/app/database/session.py
@@ -6,11 +6,30 @@ from sqlalchemy.ext.asyncio import (
 
 from app.core.config.settings import settings
 
+
+database_url = settings.DATABASE_URL
+
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1,
+    )
+
+elif database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql+asyncpg://",
+        1,
+    )
+
+
 engine = create_async_engine(
-    settings.DATABASE_URL,
+    database_url,
     echo=settings.DEBUG,
     future=True,
 )
+
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
