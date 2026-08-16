632d222cbf85b1b41e3978bc954caa7bcda3fe0d
root <root@localhost.localdomain>
2026-08-16 15:57:26 +0500

    Run web server only

--- web/run.py
+++ web/run.py
@@ -1,10 +1,9 @@
-import multiprocessing
 import os
 
 import uvicorn
 
 
-def run_web():
+if __name__ == "__main__":
     port = int(os.getenv("PORT", "8000"))
 
     uvicorn.run(
@@ -13,21 +12,3 @@ def run_web():
         port=port,
         reload=False,
     )
-
-
-def run_bot():
-    from app.bot import dp, bot
-    import asyncio
-
-    asyncio.run(dp.start_polling(bot))
-
-
-if __name__ == "__main__":
-    web_process = multiprocessing.Process(target=run_web)
-    bot_process = multiprocessing.Process(target=run_bot)
-
-    web_process.start()
-    bot_process.start()
-
-    web_process.join()
-    bot_process.join()
